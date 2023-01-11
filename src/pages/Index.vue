0a34760c3dc1a78708021a18856815ab0c6228cf
philipheferreira <philipheferreira@gmail.com>
2023-01-11 09:19:52 -0300

    primeiro commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-list bordered separator>
+      <q-item v-for="(listaTarefas, index) in listaTarefas" :key="index">
+        <q-item-section>{{ listaTarefas.titulo }}</q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex', 
+  data () {
+    return {
+      listaTarefas: [
+        {
+          titulo: 'Estudar front', 
+          feito: true
+        }, 
+        {
+          titulo: 'Almoçar direitinho', 
+          feito: false
+        },
+        {
+          titulo: 'Lembrar de fazer as atividades de tester', 
+          feito: true
+        }
+      ]
+    }
+  }
+}
+</script>
